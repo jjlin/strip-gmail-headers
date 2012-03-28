30012f30850fd1ef0c9af89967ec7950cf450943
Jeremy Lin <jjlin@cs.stanford.edu>
2012-03-27 21:22:55 -0700

    Update to handle Gmail formatting changes.

--- strip-gmail-headers.py
+++ strip-gmail-headers.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
-#
-# 05/30/2011: Initial revision.
-#
+
 import re
 import sys
 
@@ -16,24 +14,31 @@ start = html.find(target)
 end = start + len(target)
 html = html[:start] + html[end:]
 
-# Delete: First <table ...> to the next <table ...> (exclusive)
+# Delete: Next two <table> to </table>
 regex = re.compile(
-    r'('       # Begin match group.
-    r'<table ' # First opening tag.
-    r'.+?'     # Everything up to the next <table> (non-greedy).
-    r')'       # End match group.
-    r'<table ' # Next opening tag (not included in the match).
+    r'('        # Begin match group.
+    r'<table '  # Opening tag.
+    r'.+?'      # Everything up to the next </table> (non-greedy).
+    r'</table>' # Closing tag.
+    r')'        # End match group.
     ,
     re.DOTALL
 )
-match = regex.search(html)
-(start, end) = (match.start(1), match.end(1))
+for count in range(2):
+    match = regex.search(html)
+    (start, end) = (match.start(1), match.end(1))
+    html = html[:start] + html[end:]
+
+# Delete: <hr>
+target = '<hr>'
+start = html.find(target)
+end = start + len(target)
 html = html[:start] + html[end:]
 
 # Delete: Next two <tr> to </tr>
 regex = re.compile(
     r'('     # Begin match group.
-    r'<tr>'  # Opening angle bracket and tag name.
+    r'<tr>'  # Opening tag.
     r'.+?'   # Everything up to the next </tr> (non-greedy).
     r'</tr>' # Closing tag.
     r')'     # End match group.
@@ -44,12 +49,6 @@ for count in range(2):
     match = regex.search(html)
     (start, end) = (match.start(1), match.end(1))
     html = html[:start] + html[end:]
-
-# Delete: Last <hr>
-target = '<hr>'
-start = html.rfind(target)
-end = start + len(target)
-html = html[:start] + html[end:]
 
 # Write out the result.
 html_file = open(sys.argv[1], 'wb')
